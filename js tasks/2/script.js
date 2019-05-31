5b1e8c0ad32b538f35ee06d7ad2433f75f521775
greenpenguin <penguin.is.green@gmail.com>
2019-05-31 14:35:04 +0400

    Update script.js

--- js tasks/2/script.js
+++ js tasks/2/script.js
@@ -1,24 +1,27 @@
 'use strict';
-document.getElementById('btn').addEventListener('click', changeText);
 
-function changeText() {
+document.getElementById('btn').addEventListener('click', count);
+function count() {
     var str = document.getElementById('txt').value;
-    var ignore = ["?", "!", ":", ";", ",", ".", " ", "\t", "\r"];
-    var letters = {}, result;
-    var words = str.split(' ');
-
-    words.forEach(function (word) {
-        word.split('').forEach(function (letter, i) {
-            if (!letters[letter] && ignore.indexOf(letter) == -1 && -1 != word.indexOf(letter, i + 1)) {
-                letters[letter] = 1;
-            }
-        });
-    });
-
-    result = str.split('').filter(function (v) {
-        return !letters[v];
-    }).join('');
-  
-  document.getElementById("output").innerHTML = result;
-    
+    var result, i;
+    str = str.split(/([-,+,/,*])/);
+    for (i = 0; i <= str.length - 1; i++) {
+        if (str[i] === '+') {
+            str[i + 1] = parseFloat(str[i - 1]) + parseFloat(str[i + 1]);
+            result = str[i + 1];
+        }
+        if (str[i] === '-') {
+            str[i + 1] = parseFloat(str[i - 1]) - parseFloat(str[i + 1]);
+            result = str[i + 1];
+        }
+        if (str[i] === '/') {
+            str[i + 1] = parseFloat(str[i - 1]) / parseFloat(str[i + 1]);
+            result = str[i + 1];
+        }
+        if (str[i] === '*') {
+            str[i + 1] = parseFloat(str[i - 1]) * parseFloat(str[i + 1]);
+            result = str[i + 1];
+        }
+    }
+    document.getElementById("output").innerHTML = result;
 }
